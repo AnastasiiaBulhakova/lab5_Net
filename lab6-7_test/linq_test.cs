b5a611d985cae69fd5d72cea96312480b86fc2c6
Anastasiia Bulhakova <anastasiia_bulhakova@knu.ua>
2025-11-11 14:43:47 +0200

    lab7

--- lab6-7_test/linq_test.cs
+++ lab6-7_test/linq_test.cs
@@ -26,10 +26,10 @@ namespace lab6_7_test
         [TestMethod]
         public void FilterByName_ShouldReturnAllMatchingProducts_IgnoringCase()
         {
-            // Act
+
             _catalog.FilterByName("apple");
 
-            // Assert
+
             Assert.AreEqual(2, _catalog.Products.Count);
             Assert.IsTrue(_catalog.Products.All(p => p.Type.Equals("apple", System.StringComparison.OrdinalIgnoreCase)));
         }
@@ -37,10 +37,10 @@ namespace lab6_7_test
         [TestMethod]
         public void GroupByGrade_ShouldSortProductsByGrade()
         {
-            // Act
+
             _catalog.GroupByGrade();
 
-            // Assert
+
             var grades = _catalog.Products.Select(p => p.Grade).ToList();
             var sortedGrades = grades.OrderBy(g => g).ToList();
             CollectionAssert.AreEqual(sortedGrades, grades);
@@ -49,20 +49,19 @@ namespace lab6_7_test
         [TestMethod]
         public void TotalQuantity_ShouldReturnSumOfAllQuantities()
         {
-            // Act
+
             int total = _catalog.TotalQuantity();
 
-            // Assert
             Assert.AreEqual(5 + 10 + 3 + 7, total);
         }
 
         [TestMethod]
         public void TotalPrice_ShouldReturnSumOfPriceTimesQuantity()
         {
-            // Act
+
             double totalPrice = _catalog.TotalPrice();
 
-            // Assert
+
             double expected = (10 * 5) + (5 * 10) + (12 * 3) + (8 * 7);
             Assert.AreEqual(expected, totalPrice);
         }
@@ -70,14 +69,13 @@ namespace lab6_7_test
         [TestMethod]
         public void AveragePrice_ShouldReturnAveragePriceForGivenProductName()
         {
-            // Act
+
             double avgApple = _catalog.AveragePrice("apple");
             double expectedApple = (10 + 12) / 2.0;
 
             double avgBanana = _catalog.AveragePrice("Banana");
             double avgPear = _catalog.AveragePrice("Pear");
 
-            // Assert
             Assert.AreEqual(expectedApple, avgApple);
             Assert.AreEqual(5, avgBanana);
             Assert.AreEqual(0, avgPear);
